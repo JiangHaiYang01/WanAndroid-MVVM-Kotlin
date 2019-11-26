7cbbd598b0407cfe46045767d27793a9c34a0f1c
江海洋 <1885690754@163.com>
2019-11-26 17:13:04 +0800

    main act

--- app/src/main/kotlin/com/allens/ui/activity/MainActivity.kt
+++ app/src/main/kotlin/com/allens/ui/activity/MainActivity.kt
@@ -2,12 +2,33 @@ package com.allens.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.allens.model_base.base.impl.BaseMVVMAct
+import com.allens.model_base.base.impl.BaseModel
+import com.allens.model_base.base.impl.BaseVM
 import com.allens.tools.R
+import com.allens.tools.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseMVVMAct<ActivityMainBinding, MainModel, MainVM>() {
+    override fun createModel(): MainModel {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        return MainModel()
     }
+
+    override fun createVMClass(): Class<MainVM> {
+        return MainVM::class.java
+    }
+
+    override fun getContentViewId(): Int {
+        return R.layout.activity_main
+    }
+
+    override fun initMVVMListener() {
+        bind.vm = MainVM()
+    }
+
+}
+
+class MainModel : BaseModel
+
+class MainVM : BaseVM<MainModel>() {
 }
